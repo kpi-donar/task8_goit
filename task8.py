5290055fa9705d8d1a5874a19f9dc0d1190a9e3e
don-ar <reztsov.ive@gmail.com>
2022-10-07 12:25:20 +0300

    optimization

--- task8.py
+++ task8.py
@@ -2,35 +2,30 @@ import datetime
 
 
 def get_birthdays_per_week(users):
-	week_list = {}
+	week_list = {'Monday': [], 'Tuesday': [], 'Wednesday': [], 'Thursday': [], 'Friday': []}
 
 
 	for user in users:
 		d = user['birthday']
 		d = d.replace(year = datetime.date.today().year)
-		if 0 <= (d-datetime.date.today()).days <= 7:
-			if d.isoweekday() in (1,6,7):
-				if 'Monday' not in week_list:
-					week_list['Monday'] = (user['name'])
-				else:
-					week_list['Monday'] += ', ' + (user['name'])
-			else:
-				if d.strftime("%A") not in week_list:
-					week_list[d.strftime("%A")] = (user['name'])
-				else:
-					week_list[d.strftime("%A")] += ', ' + (user['name'])
+		if not 0 <= (d-datetime.date.today()).days <= 7:
+			continue
+		week_day = d.strftime("%A")
+		if week_day in ('Saturday', 'Sunday'):
+			week_day = 'Monday'
+		week_list[week_day].append(user['name'])
 
 	# printing
-
 	for key, value in week_list.items():
-		print('{:<10} {:<10}'.format(key+':', value))
+		if value:
+			print('{:<10} {:<10}'.format(key+':', ','.join(value)))
 
 def main():
 	# users defined for checking purposes
 
-	users = [{'name' : '1', 'birthday' : datetime.date(1988, 10, 3)}, {'name' : '1', 'birthday' : datetime.date(1998, 10, 3)},
-	{'name' : '2', 'birthday' : datetime.date(1988, 10, 4)}, {'name' : '2', 'birthday' : datetime.date(1998, 10, 4)},
-	{'name' : '3', 'birthday' : datetime.date(1988, 10, 5)}, {'name' : '3', 'birthday' : datetime.date(1998, 10, 5)},
+	users = [{'name' : '1', 'birthday' : datetime.date(1988, 10, 10)}, {'name' : '1', 'birthday' : datetime.date(1998, 10, 10)},
+	{'name' : '2', 'birthday' : datetime.date(1988, 10, 11)}, {'name' : '2', 'birthday' : datetime.date(1998, 10, 11)},
+	{'name' : '3', 'birthday' : datetime.date(1988, 10, 12)}, {'name' : '3', 'birthday' : datetime.date(1998, 10, 12)},
 	{'name' : '4', 'birthday' : datetime.date(1988, 10, 6)}, {'name' : '4', 'birthday' : datetime.date(1998, 10, 6)},
 	{'name' : '5', 'birthday' : datetime.date(1988, 10, 7)}, {'name' : '5', 'birthday' : datetime.date(1998, 10, 7)},
 	{'name' : '6', 'birthday' : datetime.date(1988, 10, 8)}, {'name' : '6', 'birthday' : datetime.date(1998, 10, 8)},
