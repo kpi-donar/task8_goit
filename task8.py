471a6a22b0b800b041428a22cc47f6ef667ca61a
don-ar <reztsov.ive@gmail.com>
2022-10-07 10:03:20 +0300

    update according to the mentor's comments

--- task8.py
+++ task8.py
@@ -2,30 +2,32 @@ import datetime
 
 
 def get_birthdays_per_week(users):
-	week_list = ['Monday: ', 'Tuesday: ', 'Wednesday: ', 'Thursday: ', 'Friday: ']
-	week_list_copy = week_list.copy()
-	if not users:
-		print('"Users" dict is emply')
+	week_list = {}
 
-	else:
-		for i in users:
-			d = i['birthday']
-			d = d.replace(year = datetime.date.today().year)
-			if 0 <= (d-datetime.date.today()).days <= 7:
-				if d.isoweekday() == 6 or d.isoweekday() == 7 or d.isoweekday() == 1:
-					week_list[0] = week_list[0] + i["name"] + ', '
+
+	for user in users:
+		d = user['birthday']
+		d = d.replace(year = datetime.date.today().year)
+		if 0 <= (d-datetime.date.today()).days <= 7:
+			if d.isoweekday() in (1,6,7):
+				if 'Monday' not in week_list:
+					week_list['Monday'] = (user['name'])
 				else:
-					for weekday in range(len(week_list_copy)):
-						if week_list_copy[weekday][:-2] == d.strftime("%A"):
-							week_list[weekday] = week_list[weekday] + i["name"] + ', '
+					week_list['Monday'] += ', ' + (user['name'])
+			else:
+				if d.strftime("%A") not in week_list:
+					week_list[d.strftime("%A")] = (user['name'])
+				else:
+					week_list[d.strftime("%A")] += ', ' + (user['name'])
 
-		# printing the list by weekday elements
-		for i in week_list:
-			print(i[:-2])
+	# printing
+
+	for key, value in week_list.items():
+		print('{:<10} {:<10}'.format(key+':', value))
 
 def main():
 	# users defined for checking purposes
-'''
+
 	users = [{'name' : '1', 'birthday' : datetime.date(1988, 10, 3)}, {'name' : '1', 'birthday' : datetime.date(1998, 10, 3)},
 	{'name' : '2', 'birthday' : datetime.date(1988, 10, 4)}, {'name' : '2', 'birthday' : datetime.date(1998, 10, 4)},
 	{'name' : '3', 'birthday' : datetime.date(1988, 10, 5)}, {'name' : '3', 'birthday' : datetime.date(1998, 10, 5)},
@@ -33,7 +35,7 @@ def main():
 	{'name' : '5', 'birthday' : datetime.date(1988, 10, 7)}, {'name' : '5', 'birthday' : datetime.date(1998, 10, 7)},
 	{'name' : '6', 'birthday' : datetime.date(1988, 10, 8)}, {'name' : '6', 'birthday' : datetime.date(1998, 10, 8)},
 	{'name' : '7', 'birthday' : datetime.date(1988, 10, 9)}, {'name' : '7', 'birthday' : datetime.date(1998, 10, 9)}]
-'''
+
 	get_birthdays_per_week(users)
 
 if __name__ == '__main__':
